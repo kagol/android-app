c0a015fdee356540227fb279fe0e2609d45ba1b1
kagol <kagol@sina.com>
2018-03-04 23:49:55 +0800

    add class Question

---
+++ GeoQuiz/app/src/main/java/io/github/kagol/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package io.github.kagol.geoquiz;
+
+/**
+ * Created by Administrator on 2018/3/4.
+ */
+
+public class Question {
+    private int mTextId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextId =textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextId() {
+        return mTextId;
+    }
+
+    public void setTextId(int textId) {
+        mTextId = textId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
