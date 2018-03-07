1e822fbaac76a3dad48415535e8f1272e01c8f08
Kagol <Kagol>
2018-03-07 23:34:05 +0800

    add next button

--- GeoQuiz/app/src/main/java/io/github/kagol/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/io/github/kagol/geoquiz/QuizActivity.java
@@ -26,22 +26,42 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkQuestion(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextId();
-        mQuestionTextView.setText(question);
+        // int question = mQuestionBank[mCurrentIndex].getTextId();
+        // mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                // Toast.makeText(QuizActivity.this,
+                //         R.string.incorrect_toast,
+                //         Toast.LENGTH_SHORT).show();
+                checkQuestion(true);
             }
         });
 
@@ -49,10 +69,24 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                // Toast.makeText(QuizActivity.this,
+                //         R.string.correct_toast,
+                //         Toast.LENGTH_SHORT).show();
+                checkQuestion(false);
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                // int question = mQuestionBank[mCurrentIndex].getTextId();
+                // mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
     }
 }
