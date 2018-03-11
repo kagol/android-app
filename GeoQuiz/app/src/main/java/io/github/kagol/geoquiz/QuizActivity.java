b310ffd71ae1b002b64e3e9e626a41de1ebafb2e
kagol <kagol@sina.com>
2018-03-12 00:00:34 +0800

    add CheatActivity

--- GeoQuiz/app/src/main/java/io/github/kagol/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/io/github/kagol/geoquiz/QuizActivity.java
@@ -1,5 +1,7 @@
 package io.github.kagol.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.media.Image;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +20,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -26,6 +29,8 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mPrevImgButton;
     private ImageButton mNextImgButton;
     private TextView mQuestionTextView;
+
+    private Button mCheatButton;
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
@@ -36,6 +41,7 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextId();
@@ -51,6 +57,24 @@ public class QuizActivity extends AppCompatActivity {
             messageResId = R.string.correct_toast;
         } else {
             messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(mIsCheater) {
+            messageResId = R.string.judgment_toast;
+        } else {
+            if(userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
@@ -105,6 +129,7 @@ public class QuizActivity extends AppCompatActivity {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 // int question = mQuestionBank[mCurrentIndex].getTextId();
                 // mQuestionTextView.setText(question);
+                mIsCheater = false;
                 updateQuestion();
             }
         });
@@ -131,7 +156,40 @@ public class QuizActivity extends AppCompatActivity {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity startActivity(Intent)
+                // 基于intent的通信
+                // Component: Activity/Service/Broadcast receiver/Content provider
+
+                // 显式intent（同一个应用里），不同应用之间通信用隐式intent
+                // packageContext Context: A Context of the application package implementing this class.
+                // cls Class: The component class that is to be used for the intent.
+                // Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                // startActivity(i);
+                startActivityForResult(i, REQUEST_CODE_CHEAT);
+            }
+        });
+
         updateQuestion();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if(requestCode == REQUEST_CODE_CHEAT) {
+            if(data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 
     @Override
